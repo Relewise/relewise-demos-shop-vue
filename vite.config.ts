9f82147ea1b9481065a04387d522977167797e78
Martin Zanoni <mza@relewise.com>
2023-05-10 09:46:08 +0200

    add base url for prod

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), vueJsx()],
@@ -12,5 +14,6 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url)),
     
         },  
-    },   
+    },
+    base: isProd ? '/relewise-demos-shop/' : undefined,  
 });
