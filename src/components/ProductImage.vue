a9baaa993ffb92e2b2f08a9d8ac75e2839abb12d
SWH-Relewise <swh@relewise.com>
2023-09-15 10:35:29 +0200

    Add popular products web component

--- src/components/ProductImage.vue
+++ src/components/ProductImage.vue
@@ -1,16 +1,17 @@
 <template>
     <img v-if="!error"
          class="object-cover"
-         :src="image"
+         :src="findImage(product)"
          alt="product image"
          @error="error=true">
     <PhotoIcon v-else class="h-full w-full text-zinc-300"/>
 </template>
 
 <script setup lang="ts">
-import type { DataValue, ProductResult } from '@relewise/client';
-import { computed, toRefs, type PropType, ref } from 'vue';
 import { PhotoIcon } from '@heroicons/vue/24/outline';
+import type { ProductResult } from '@relewise/client';
+import { ref, toRefs, type PropType } from 'vue';
+import { findImage } from '@/services/image.service';
 
 const props = defineProps({
     product: { type: Object as PropType<ProductResult>, required: true },
@@ -18,27 +19,4 @@ const props = defineProps({
 
 const { product } = toRefs(props);
 const error = ref(false);
-const image = computed(() => {
-
-    return mapDataKey(product.value.data ?? {}) ??
-        mapDataKey(product.value.variant?.data ?? {}) ??
-        '';
-});
-
-function mapDataKey(data: Record<string, DataValue>) {
-    for (const dataKey of Object.keys(data ?? {})) {
-
-        if (dataKey.toLowerCase().includes('image')) {
-            const value = data[dataKey];
-
-            if (value.type === 'String') {
-                return value.value;
-            } else if (value.type === 'StringList') {
-                return value.value.$values[0];
-            }
-        }
-    }
-
-    return null;
-}
 </script>
---
+++ src/components/WebComponentProductTemplate.ts
@@ -0,0 +1,128 @@
+import { findImage } from '@/services/image.service';
+import type { ProductResult } from '@relewise/client';
+import type { TemplateExtensions } from '@relewise/web-components';
+
+export const WebComponentProductTemplate = (product: ProductResult, extensions: TemplateExtensions) => {
+    return extensions.html`
+        <style>
+            a {
+                text-decoration: none;
+                color: inherit;
+            }
+
+            img {
+                max-width: 100%;
+                height: auto;
+                display: block;
+                vertical-align: middle;
+            }
+
+            .product-link {
+                display: flex; 
+                overflow: hidden; 
+                position: relative; 
+                padding-top: 0.75rem;
+                padding-bottom: 0.75rem; 
+                flex-direction: column; 
+                border-radius: 0.25rem; 
+                background-color: #ffffff; 
+            }
+
+            .image-container {
+                display: flex; 
+                overflow: hidden; 
+                position: relative; 
+                margin-left: 0.75rem;
+                margin-right: 0.75rem; 
+                justify-content: center; 
+                height: 275px;
+            }
+
+            .image {
+                object-fit: cover;
+            }
+
+            .on-sale {
+                position: absolute; 
+                top: 0; 
+                left: 0; 
+                padding-left: 0.5rem;
+                padding-right: 0.5rem; 
+                margin: 0.5rem; 
+                border-radius: 9999px; 
+                font-size: 0.875rem;
+                line-height: 1.25rem; 
+                font-weight: 500; 
+                text-align: center; 
+                color: #ffffff; 
+                background-color: #000000; 
+            }
+
+            .padding {
+                padding-left: 0.75rem;
+                padding-right: 0.75rem; 
+                margin-top: 0.75rem; 
+            }
+
+            .text-left {
+                text-align: left;
+            }
+
+            .brand {
+                color: #71717a;
+                font-size: 0.875rem;
+                line-height: 1.25rem; 
+            }
+
+            .display-name {
+                height: 2.5rem; 
+                font-weight: 600; 
+                letter-spacing: -0.025em; 
+                line-height: 1.25rem; 
+                font-size: 1rem;
+                margin-top: 0.5rem;
+                margin-bottom: 0;
+            }
+
+            .price-container {
+                display: flex; 
+                margin-top: 0.5rem; 
+                justify-content: space-between; 
+                align-items: center; 
+            }
+
+            .sales-price {
+                margin-right: 0.25rem; 
+                font-size: 1.125rem;
+                line-height: 1.75rem; 
+                font-weight: 600; 
+                line-height: 1; 
+            }
+            
+            .list-price {
+                text-decoration: line-through; 
+            }
+
+        </style>
+        <a href="/product/${product.productId}" class="product-link">
+            <div class="image-container">
+                <img src="${findImage(product)}" class="image"/>
+                ${product.salesPrice !== product.listPrice ? extensions.html`<span class="on-sale">ON SALE</span>` :extensions.html``}
+            </div>
+            <div class="padding">
+            <div class="text-left">
+                ${product.brand ? extensions.html`<span class="brand">${product.brand.displayName}</span>` : ''}
+                <h5 class="display-name">
+                    ${product.displayName}
+                </h5>
+            </div>
+            <div class="price-container">
+                <p>
+                    <span class="sales-price">${extensions.helpers.formatPrice(product.salesPrice)}</span>
+                    ${product.salesPrice !== product.listPrice ? extensions.html`<span class="list-price">${extensions.helpers.formatPrice(product.listPrice)}</span>` : ''}
+                </p>
+            </div>
+        </div>
+        </a>
+    `;
+};
