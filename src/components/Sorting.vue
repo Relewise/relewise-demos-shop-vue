6172d38c04ecd4900703f9e0db2dde866e1572ee
Simon Walenkamp Hansen <swh@relewise.com>
2024-06-19 11:29:29 +0200

    Use correct type

--- src/components/Sorting.vue
+++ src/components/Sorting.vue
@@ -16,17 +16,17 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, toRefs } from 'vue';
+import { computed, toRefs, type PropType } from 'vue';
 
 const props = defineProps({
-    modelValue: { type: String, required: true },
+    modelValue: { type: Object as PropType<string | string[]>, required: true },
 });
 
 const { modelValue } = toRefs(props);
 
 const emit = defineEmits(['update:modelValue']);
 
-const internalSelectedOption = computed<string>({
+const internalSelectedOption = computed<string | string[]>({
     get: () => modelValue.value,
     set: (v) => emit('update:modelValue', v),
 });
