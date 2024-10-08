def5d6840e85259915956dabfee35e3a4b742958
Simon Walenkamp Hansen <swh@relewise.com>
2024-10-08 14:57:15 +0200

    weird

--- src/components/ProductTile.vue
+++ src/components/ProductTile.vue
@@ -27,7 +27,7 @@ const { product } = toRefs(props);
                     <template #content>
                         <div class="w-96">
                             <div class="flex flex-col">
-                                <div class="font-semibold px-2 first-letter:bg-gray-100 py-2">
+                                <div class="font-semibold px-2 bg-gray-100 py-2">
                                     Retail Media
                                 </div>
                                 <hr class="p-0 my-0">
