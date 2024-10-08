24c84b365c99e84048cf38bfa83c5a65ebf4a915
Simon Walenkamp Hansen <swh@relewise.com>
2024-10-08 14:54:18 +0200

    Review comments

--- src/components/ProductTile.vue
+++ src/components/ProductTile.vue
@@ -27,11 +27,11 @@ const { product } = toRefs(props);
                     <template #content>
                         <div class="w-96">
                             <div class="flex flex-col">
-                                <div class="font-semibold px-2 text-14 bg-gray-100 py-2">
+                                <div class="font-semibold px-2 first-letter:bg-gray-100 py-2">
                                     Retail Media
                                 </div>
                                 <hr class="p-0 my-0">
-                                <div class="p-2 text-13">
+                                <div class="p-2">
                                     This product is highlighted by our supplier as it could be relevant or interesting for you.
                                 </div>
                             </div>
