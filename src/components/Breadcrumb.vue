3951d045bca31cb4bb7b23466ba4d4f094910e61
Simon Walenkamp Hansen <swh@relewise.com>
2024-10-31 06:07:59 +0100

    Refactor

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -10,21 +10,10 @@
             :key="index" 
             class="flex items-center">
             <span class="mx-2">/</span>
-            <RouterLink v-if="index === 0"
-                        :to="{ name: 'category', params: { id: category.id } }"
-                        class="text-zinc-700 whitespace-nowrap hover:text-brand-500 cursor-pointer">
+            <RouterLink
+                :to="generateRoute(index) ?? '/'"
+                class="text-zinc-700 whitespace-nowrap hover:text-brand-500 cursor-pointer">
                 {{ category.displayName ?? category.id }}
-            </RouterLink>
-            <RouterLink v-if="index === 1"
-                        :to="{ name: 'sub-category', params: { parent: breadcrumb[0].id, id: category.id } }"
-                        class="text-zinc-700 whitespace-nowrap hover:text-brand-500 cursor-pointer">
-                {{ category.displayName ?? category.id }}
-            </RouterLink>
-
-            <RouterLink v-if="index === 2"
-                        :to="{ name: 'sub-sub-category', params: { grand: breadcrumb[0].id, parent: breadcrumb[1].id, id: category.id } }"
-                        class="text-zinc-700 whitespace-nowrap hover:text-brand-500 cursor-pointer">
-                {{ category.displayName ?? category.id }} 
             </RouterLink>
         </div>
     </div>
@@ -35,10 +24,33 @@ import type { CategoryNameAndIdResult } from '@relewise/client';
 import { toRefs, type PropType } from 'vue';
 import { HomeIcon } from '@heroicons/vue/24/outline';
 import contextStore from '@/stores/context.store';
+import type { RouteLocationAsRelativeGeneric } from 'vue-router';
 
 const props = defineProps({
     breadcrumb: { type: Object as PropType<CategoryNameAndIdResult[]>, required: true },
 });
 
 const { breadcrumb } = toRefs(props);
+
+const generateRoute = (index: number): RouteLocationAsRelativeGeneric | undefined   => {
+    switch(index) {
+    case 0: return { 
+        name: 'category',
+        params: { id: breadcrumb.value[index].id }, 
+    };
+    case 1: return {
+        name: 'sub-category',
+        params: { 
+            parent: breadcrumb.value[0].id,
+            id: breadcrumb.value[index].id }, 
+    };
+    case 2: return { 
+        name: 'sub-sub-category', 
+        params: { 
+            grand: breadcrumb.value[0].id,
+            parent: breadcrumb.value[1].id,
+            id: breadcrumb.value[index].id },
+    };
+    }
+};
 </script>
