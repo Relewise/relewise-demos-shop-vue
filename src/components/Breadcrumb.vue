1d9f293bd10302a41a9dc7e517d0e6bd27644ccc
Martin Zanoni <mza@relewise.com>
2024-11-01 08:17:53 +0100

    different way for building the breadcrumb

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -1,35 +1,24 @@
 <template>
-    <div class="flex py-2">
-        <RouterLink 
-            to="/" 
-            class="text-zinc-700 whitespace-nowrap hover:text-brand-500 cursor-pointer flex items-center">
-            <HomeIcon class="h-5 w-5"/>
-        </RouterLink>
-        <div 
-            v-for="(category, index) in breadcrumb.slice(0, contextStore.context.value.allowThirdLevelCategories ? 3 : 2)" 
-            :key="index" 
-            class="flex items-center">
-            <span class="mx-2">/</span>
-            <RouterLink
-                :to="generateRoute(index) ?? '/'"
-                class="text-zinc-700 whitespace-nowrap hover:text-brand-500 cursor-pointer">
-                {{ category.displayName ?? category.id }}
+    <div class="flex py-2 gap-2">
+        <template v-for="(el, index) in nav" :key="el.name">
+            <RouterLink 
+                :to="el.route"
+                class="text-zinc-700 whitespace-nowrap hover:text-brand-500 cursor-pointer flex items-center">
+                <template v-if="el.name === 'Home'">
+                    <HomeIcon class="h-5 w-5"/>
+                </template> 
+                <template v-else>
+                    {{ el.name }}
+                </template>
             </RouterLink>
-        </div>
-        <div v-if="product">
-            <span class="mx-2">/</span>
-            <RouterLink
-                :to="{ name: 'product', params: { id: product.productId } }"
-                class="text-zinc-700 whitespace-nowrap hover:text-brand-500 cursor-pointer">
-                {{ product.displayName ?? product.productId }}
-            </RouterLink>
-        </div>
+            <span v-if="index !== nav.length - 1">/</span>
+        </template>
     </div>
 </template>
 
 <script setup lang="ts">
 import type { CategoryNameAndIdResult, ProductResult } from '@relewise/client';
-import { toRefs, type PropType } from 'vue';
+import { computed, toRefs, type PropType } from 'vue';
 import { HomeIcon } from '@heroicons/vue/24/outline';
 import contextStore from '@/stores/context.store';
 import type { RouteLocationAsRelativeGeneric } from 'vue-router';
@@ -41,24 +30,48 @@ const props = defineProps({
 
 const { breadcrumb } = toRefs(props);
 
-const generateRoute = (index: number): RouteLocationAsRelativeGeneric | undefined   => {
-    switch(index) {
-    case 0: return { 
+const nav = computed(() => {
+    const path: { name: string | null | undefined, route: RouteLocationAsRelativeGeneric }[] = [
+        { name: 'Home', route: { name: 'home' } },
+    ];
+
+    for (const [i, category] of breadcrumb.value.slice(0, contextStore.context.value.allowThirdLevelCategories ? 3 : 2).entries()) {
+        path.push({
+            name: category.displayName ?? category.id ?? '',
+            route: generateRoute(i) ?? { name: 'home' },
+        });
+    }
+
+    if (props.product) {
+        path.push({
+            name: props.product.displayName ?? props.product.productId,
+            route: { name: 'product', params: { id: props.product.productId } },
+        });
+    }
+
+    return path;
+});
+
+const generateRoute = (index: number): RouteLocationAsRelativeGeneric | undefined => {
+    switch (index) {
+    case 0: return {
         name: 'category',
-        params: { id: breadcrumb.value[index].id }, 
+        params: { id: breadcrumb.value[index].id },
     };
     case 1: return {
         name: 'sub-category',
-        params: { 
+        params: {
             parent: breadcrumb.value[0].id,
-            id: breadcrumb.value[index].id }, 
+            id: breadcrumb.value[index].id,
+        },
     };
-    case 2: return { 
-        name: 'sub-sub-category', 
-        params: { 
+    case 2: return {
+        name: 'sub-sub-category',
+        params: {
             grand: breadcrumb.value[0].id,
             parent: breadcrumb.value[1].id,
-            id: breadcrumb.value[index].id },
+            id: breadcrumb.value[index].id,
+        },
     };
     }
 };
