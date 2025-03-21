b664e4e8e8af323f832d53bbeb7b2e0b865b9ebd
Christian Bennich <cbe@relewise.com>
2025-03-21 17:01:51 +0100

    update null search scenario to consider multiple terms

--- src/components/DemoScenarios.vue
+++ src/components/DemoScenarios.vue
@@ -47,9 +47,8 @@ async function generateSearchTermPredictions() {
     const searcher = contextStore.getSearcher();
     const tracker = contextStore.getTracker();
 
-
-
     userClassifications.forEach(classification => {
+        console.log(classification);
         for (let index = 0; index < 30; index++) {
 
             const { user, settings } = createUserAndSettings(classification);
@@ -112,30 +111,35 @@ async function generateNullSearcScenario() {
     const searcher = contextStore.getSearcher();
     const tracker = contextStore.getTracker();
 
+    const baseTerm = "somethings";
+    const termVariants = Array.from({ length: baseTerm.length - 2 }, (_, i) => baseTerm.slice(0, i + 3));
+
     userClassifications.forEach(classification => {
-        for (let index = 0; index < 100; index++) {
-            const { user, settings } = createUserAndSettings(classification);
+        termVariants.forEach(term => {
+            for (let index = 0; index < 100; index++) {
+                const { user, settings } = createUserAndSettings(classification);
 
-            const builder = new ProductSearchBuilder(settings)
-                .setSelectedProductProperties({ displayName: true })
-                .setTerm('something')
-                .pagination(
-                    p => p
-                        .setPageSize(100)
-                        .setPage(1)
-                )
+                const builder = new ProductSearchBuilder(settings)
+                    .setSelectedProductProperties({ displayName: true })
+                    .setTerm(term)
+                    .pagination(
+                        p => p
+                            .setPageSize(100)
+                            .setPage(1)
+                    );
 
-            searcher.searchProducts(builder.build());
+                searcher.searchProducts(builder.build());
 
-            tracker.trackProductView({ productId: 'b2f4d740-2d93-4b5b-afb1-209e4c74a1e0', user: user }); //Dell UltraVision 4K
-            tracker.trackProductView({ productId: 'ab30db73-b70f-4e31-b0d9-62a5fe22c087', user: user }); //Canon EOS M50 Mark II 
-            tracker.trackProductView({ productId: 'bd7bbc98-f3cf-4205-8d24-2ae4c3fdf40a', user: user }); //Samsung SoundBooster Mini 
-            tracker.trackProductView({ productId: 'a6d2f764-90b5-4cfc-a030-9e5fa0dcb6e4', user: user }); //Bose Smartband 1
-            tracker.trackProductView({ productId: 'f98b4d52-8ba9-42a1-9d76-5a5abb5b1d09', user: user }); //Samsung DW60R8070BB 
-            tracker.trackProductView({ productId: 'de208aea-cb04-4dd5-a290-4ef78d9829b5', user: user }); //Pro-Ject Essential III
-            tracker.trackProductView({ productId: '3771701a-ef82-4a05-bc15-d83e25a7378b', user: user }); //Canon PIXMA TR8620
-            tracker.trackProductView({ productId: 'e6c8d3b0-ff48-4934-97e8-dde0b6c7buns', user: user }); //Logitech MX Vertical
-        }
+                tracker.trackProductView({ productId: 'b2f4d740-2d93-4b5b-afb1-209e4c74a1e0', user: user }); //Dell UltraVision 4K
+                tracker.trackProductView({ productId: 'ab30db73-b70f-4e31-b0d9-62a5fe22c087', user: user }); //Canon EOS M50 Mark II 
+                tracker.trackProductView({ productId: 'bd7bbc98-f3cf-4205-8d24-2ae4c3fdf40a', user: user }); //Samsung SoundBooster Mini 
+                tracker.trackProductView({ productId: 'a6d2f764-90b5-4cfc-a030-9e5fa0dcb6e4', user: user }); //Bose Smartband 1
+                tracker.trackProductView({ productId: 'f98b4d52-8ba9-42a1-9d76-5a5abb5b1d09', user: user }); //Samsung DW60R8070BB 
+                tracker.trackProductView({ productId: 'de208aea-cb04-4dd5-a290-4ef78d9829b5', user: user }); //Pro-Ject Essential III
+                tracker.trackProductView({ productId: '3771701a-ef82-4a05-bc15-d83e25a7378b', user: user }); //Canon PIXMA TR8620
+                tracker.trackProductView({ productId: 'e6c8d3b0-ff48-4934-97e8-dde0b6c7buns', user: user }); //Logitech MX Vertical
+            }
+        });
     });
 }
 
