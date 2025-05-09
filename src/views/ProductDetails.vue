c1578b3c216e527f6654a8f0d759effeee206bac
Christian Bennich <cbe@relewise.com>
2025-05-09 11:40:52 +0200

    moved PDP variant grid to separate component

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -83,53 +83,7 @@
             </div>
         </div>
 
-        <div>
-            <template v-if="product?.filteredVariants?.length">
-                <div class="my-3">
-                    <div class="text-2xl font-semibold">
-                        Showing {{ Math.min(product.filteredVariants.length, 10) }} of {{
-                            product.filteredVariants.length }} variants
-                    </div>
-                    <table class="w-full mt-4 border-t border-gray-200 text-left text-sm">
-                        <thead class="bg-gray-50 text-gray-700 uppercase">
-                            <tr>
-                                <th class="py-2 px-3">Image</th>
-                                <th class="py-2 px-3">Product name</th>
-                                <th class="py-2 px-3">Variant Id</th>
-                                <th class="py-2 px-3">Availability</th>
-                                <th class="py-2 px-3">Price</th>
-                                <th class="py-2 px-3">Price incl. VAT</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="(variant, index) in product.filteredVariants.slice(0, 10)" :key="index" class="border-b border-gray-200">
-                                <td class="py-2 px-3">
-                                    <img :src="variant.data?.Image?.value" alt="Variant Image"
-                                        class="w-12 h-12 object-contain" />
-                                </td>
-                                <td class="py-2 px-3">
-                                    {{ variant.displayName }}
-                                </td>
-                                <router-link :to="{ path: $route.path, query: { variantId: variant.variantId } }" class="text-blue-600 underline">
-                                        {{ variant.variantId }}
-                                    </router-link>
-                                <td class="py-2 px-3 text-green-600">
-                                    In stock
-                                </td>
-                                <td class="py-2 px-3 font-semibold">
-                                    EUR {{variant.listPrice??product.listPrice}}
-                                </td>
-                                <td class="py-2 px-3 font-semibold">
-                                    EUR {{variant.salesPrice??product.salesPrice}}
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-
-
-            </template>
-        </div>
+        <VariantGridDisplay :product="product" />
 
         <div v-if="product!.data && product!.data.SoldOut && product!.data.SoldOut.value == 'true'">
             <div class="my-3">
@@ -180,6 +134,7 @@ import ProductImage from '../components/ProductImage.vue';
 import Breadcrumb from '../components/Breadcrumb.vue';
 import { addAssortmentFilters, addCartFilter } from '@/stores/customFilters';
 import ProductTile from '../components/ProductTile.vue';
+import VariantGridDisplay from '../components/VariantGridDisplay.vue'
 
 const productId = ref<string>('');
 const product = ref<ProductResult | null | undefined>(null);
