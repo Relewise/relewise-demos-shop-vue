20ab1fff32fc2570ae81239a92cf3f6734af56fe
mkr-relewise <mkr@relewise.com>
2025-10-22 10:20:41 +0200

    chore: lock vite dev server to port 5173

--- vite.config.ts
+++ vite.config.ts
@@ -26,5 +26,9 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
+    server: {
+        port: 5173,
+        strictPort: true,
+    },
     base: undefined,
 });
