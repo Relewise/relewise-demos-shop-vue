209576a3c4055ee6ea00b692501c840164c708ec
Martin Zanoni <mza@relewise.com>
2025-11-25 13:51:37 +0100

    add paging

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -3,7 +3,11 @@
         <h1 class="text-xl lg:text-4xl font-semibold my-6 underline--yellow inline-block">Your Favorites</h1>
 
         <div class="grid gap-2 xl:gap-6 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-3" v-if="results">
-            <ProductTile v-for="(product, index) in results" :key="index" :product="product"></ProductTile>
+            <ProductTile v-for="(product, index) in results.results" :key="index" :product="product"></ProductTile>
+        </div>
+
+        <div class="py-3 flex justify-center mt-10" v-if="results">
+            <Pagination v-model.sync="page" v-model:total="results.hits" :page-size="40" />
         </div>
 
         <div v-if="userIsAnonymous(contextStore.user.value)">
@@ -15,26 +19,36 @@
 <script setup lang="ts">
 import ProductTile from '@/components/ProductTile.vue';
 import contextStore from '@/stores/context.store';
-import { ProductSearchBuilder, userIsAnonymous, type ProductResult } from '@relewise/client';
-import { onMounted, ref } from 'vue';
+import { ProductSearchBuilder, userIsAnonymous, type ProductSearchResponse } from '@relewise/client';
+import { onMounted, ref, watch } from 'vue';
 
-const results = ref<ProductResult[]>([]);
+const results = ref<ProductSearchResponse>();
+const page = ref(1);
 
 onMounted(async () => {
     // If the user is anonymous, do not attempt to load favorites as they won't have any.
     if (userIsAnonymous(contextStore.user.value)) {
-        results.value = [];
+        results.value = undefined;
         return;
     }
 
+    await search();
+});
+
+async function search() {
     const builder = new ProductSearchBuilder(contextStore.defaultSettings)
         .setSelectedProductProperties(contextStore.selectedProductProperties)
         .setSelectedVariantProperties({ allData: true })
         .setExplodedVariants(1)
+        .pagination(p => p.setPage(page.value).setPageSize(40))
         .filters(f => f.addProductEngagementFilter({ isFavorite: true }));
 
     const searcher = await contextStore.getSearcher();
 
-    results.value = (await searcher.searchProducts(builder.build()))?.results ?? [];
+    results.value = (await searcher.searchProducts(builder.build()));
+}
+
+watch(page, async () => {
+    await search();
 });
 </script>
