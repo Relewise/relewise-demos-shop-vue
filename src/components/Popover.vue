7861fc393eed4baf92702e1ddc215c8662b5d20f
mkr-relewise <mkr@relewise.com>
2025-11-26 15:52:11 +0100

    refactor: cleanup

--- src/components/Popover.vue
+++ src/components/Popover.vue
@@ -1,51 +1,40 @@
 <template>
-    <VDropdown
-        :placement="placement"
-        :triggers="['click']"
-        :container="container"
-        :arrow="arrow"
-        :distance="distance"
-        :popper-class="popperClass"
-    >
+    <VDropdown :placement="placement" :arrow="arrow" :triggers="['click']">
         <slot></slot>
-
         <template #popper>
             <slot name="content" />
         </template>
     </VDropdown>
 </template>
-  
+
 <script lang="ts" setup>
 import { type PropType, toRefs } from 'vue';
 import { Dropdown as VDropdown } from 'floating-vue';
 
 // Values taken from here: https://floating-vue.starpad.dev/api/#placement
-export type Placement = 'auto' 
- | 'auto-start'
- | 'auto-end'
- | 'top'
- | 'top-start'
- | 'top-end'
- | 'right'
- | 'right-start'
- | 'right-end'
- | 'bottom'
- | 'bottom-start'
- | 'bottom-end'
- | 'left'
- | 'left-start'
- | 'left-end';
+export type Placement = 'auto'
+    | 'auto-start'
+    | 'auto-end'
+    | 'top'
+    | 'top-start'
+    | 'top-end'
+    | 'right'
+    | 'right-start'
+    | 'right-end'
+    | 'bottom'
+    | 'bottom-start'
+    | 'bottom-end'
+    | 'left'
+    | 'left-start'
+    | 'left-end';
 
 type ContainerTarget = string | HTMLElement | false;
 
 const props = defineProps({
     placement: { type: String as PropType<Placement>, default: 'bottom-start' },
-    container: { type: [String, Boolean, Object] as PropType<ContainerTarget>, default: undefined },
     arrow: { type: Boolean as PropType<boolean | undefined>, default: undefined },
-    distance: { type: Number as PropType<number | undefined>, default: undefined },
-    popperClass: { type: String as PropType<string | undefined>, default: undefined },
 });
-const { placement, container, arrow, distance, popperClass } = toRefs(props);
+const { placement, arrow } = toRefs(props);
 </script>
 
 <style lang="css">
